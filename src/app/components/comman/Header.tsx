007f417683a763057e8562bc0c5ecb6ebc68ca64
sachinkumar9849 <englishjha@gmail.com>
2025-05-03 21:53:41 +0545

    team page name change

--- src/app/components/comman/Header.tsx
+++ src/app/components/comman/Header.tsx
@@ -164,7 +164,7 @@ const Header = () => {
                                         </li>
                                         <li>
                                             <Link
-                                                href="/team"
+                                                href="/faculties"
                                                 className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
                                             >
                                                 Faculties
